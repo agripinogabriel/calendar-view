831913840d08412e564eb459b15171e85f9a80b9
Agripino Gabriel <agripinogabriel@gmail.com>
2021-05-23 23:12:31 -0300

    chore: User Logger on sample app

--- Sample/Shared/ContentView.swift
+++ Sample/Shared/ContentView.swift
@@ -5,7 +5,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             CalendarView { date in
-                print(formatDate(date))
+                Logger.logInfo(formatDate(date), scope: "CalendarViewSample")
             }
             Spacer()
         }
