9c5743bbac255f5da0aa333f826010dbf88b01d7
Agripino Gabriel <agripinogabriel@gmail.com>
2021-05-23 23:09:34 -0300

    chore: Create Logger

---
+++ CalendarView/Sources/CalendarView/Logger/LogLevel.swift
@@ -0,0 +1,18 @@
+public enum LogLevel: Int {
+    case off = 0, error, warning, info, debug
+
+    func description() -> String {
+        switch self {
+        case .debug:
+            return "DEBUG"
+        case .info:
+            return "INFO"
+        case .warning:
+            return "WARNING"
+        case .error:
+            return "ERROR"
+        default:
+            return ""
+        }
+    }
+}
