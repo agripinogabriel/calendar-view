28b474ac14367cefa5ea18d8e2ccd75f0ee60e51
Agripino Gabriel <agripinogabriel@gmail.com>
2021-05-26 00:20:52 -0300

    feat: Shows days of month

--- CalendarView/Sources/CalendarView/Views/MonthView.swift
+++ CalendarView/Sources/CalendarView/Views/MonthView.swift
@@ -16,8 +16,44 @@ struct MonthView: View {
                             .frame(width: itemWidth, alignment: .center)
                     }
                 }
+                ForEach(0..<6) { weekOfMonth in
+                    VStack {
+                        HStack(spacing: 0) {
+                            ForEach(0..<7) { dayOfMonth in
+                                let day = dateFor(weekOfMonth: weekOfMonth, dayOfMonth: dayOfMonth)
+                                let isSelected = Calendar.current.isDate(day, inSameDayAs: date)
+                                let opacity = opacity(for: day, selected: isSelected)
+                                let color = isSelected ? Color(.darkGray) : Color(.lightGray)
+                                numberView(day.day, color: color, opacity: opacity)
+                                    .frame(width: itemWidth, alignment: .center)
+                            }
+                        }
+                    }
+                }
             }
         }
+    }
+    
+    private func dateFor(weekOfMonth: Int, dayOfMonth: Int) -> Date {
+        let firstDayOnMonth = date.firstDayOnMonth
+        let fisrtDayOnWeek = firstDayOnMonth.fisrtDayOnWeek
+        let factor = (weekOfMonth * 7) + dayOfMonth
+        return Calendar.current.date(byAdding: .day, value: factor, to: fisrtDayOnWeek)!
+    }
+    
+    private func opacity(for date: Date, selected: Bool) -> Double {
+        guard !selected else { return 1.0 }
+        guard date.isSameMonth(of: self.date) else { return 0.3 }
+        guard date.isWeekend else { return 1.0 }
+        return 0.7
+    }
+    
+    private func numberView(_ num: Int, color: Color, opacity: Double) -> some View {
+        Text("\(num)")
+            .font(.system(size: 22, weight: .black, design: .rounded))
+            .foregroundColor(color)
+            .lineLimit(1)
+            .opacity(opacity)
     }
     
     private func textView(_ text: String) -> some View {
