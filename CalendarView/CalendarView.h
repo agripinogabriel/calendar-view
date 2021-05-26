af92e52cf5f74100b213a49851647c03ac1a3202
Agripino Gabriel <agripinogabriel@gmail.com>
2021-05-26 08:44:41 -0300

    chore: Move code to a framework project

---
+++ CalendarView/CalendarView.h
@@ -0,0 +1,18 @@
+//
+//  CalendarView.h
+//  CalendarView
+//
+//  Created by Agripino Damasceno on 26/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalendarView.
+FOUNDATION_EXPORT double CalendarViewVersionNumber;
+
+//! Project version string for CalendarView.
+FOUNDATION_EXPORT const unsigned char CalendarViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarView/PublicHeader.h>
+
+
--- CalendarView/Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import CalendarViewTests
-
-var tests = [XCTestCaseEntry]()
-tests += CalendarViewTests.allTests()
-XCTMain(tests)
