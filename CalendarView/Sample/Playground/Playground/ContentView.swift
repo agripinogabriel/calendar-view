f69987f5d6ee6a5ed5b0d9cb6559a40d6363bc81
Agripino Gabriel <agripinogabriel@gmail.com>
2021-05-26 08:58:24 -0300

    chore: Add sample app to framework project

---
+++ CalendarView/Sample/Playground/Playground/ContentView.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+import CalendarView
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            CalendarView (date: Calendar.dayFirstSunshine, style: .day) { date in
+                Logger.logInfo(formatDate(date), scope: "CalendarViewSample")
+            }
+            .padding()
+            CalendarView (date: Calendar.dayFirstSunshine, style: .week) { date in
+                Logger.logInfo(formatDate(date), scope: "CalendarViewSample")
+            }
+            .padding()
+            CalendarView (date: Calendar.dayFirstSunshine, style: .month) { date in
+                Logger.logInfo(formatDate(date), scope: "CalendarViewSample")
+            }
+            .padding()
+            Spacer()
+        }
+    }
+    
+    private func formatDate(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .full
+        return dateFormatter.string(from: date)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+extension Calendar {
+    static var dayFirstSunshine: Date {
+        let dateComponents = DateComponents.init(year: 1987, month: 12, day: 21)
+        let dayFirstSunshine = Calendar.current.date(from: dateComponents)!
+        return dayFirstSunshine
+    }
+}
